08ee9b7d597e94f196554efc26ab5e542be4588b
MomenMorgan <mominmorgan10@gmail.com>
2023-11-27 19:46:02 +0200

    updatecat

--- src/components/reecipes/Catlist.js
+++ src/components/reecipes/Catlist.js
@@ -1,26 +1,40 @@
-import { useLoaderData, Link } from "react-router-dom"
+import { useLoaderData, Link } from "react-router-dom";
 
 export default function Catlist() {
-  const data = useLoaderData()
+  const data = useLoaderData();
   console.log(data);
   return (
-    <div> 
-      {
-      data.documents.map(el => (
-      <div key={el.id}>
-        <Link to={`/details`}>{el.name}</Link>
-      </div>
-    
+    <div className="bg-slate-400 p-8">
+      <h1 className="pb-4">Categories</h1>
 
-    ))}</div>
-  )
+      <h2 className="pt-4">Cusine</h2>
+      {data.documents.map((el) => (
+        <div key={el.id} className="pt-2">
+          <Link to={`/details`}>{el.name}</Link>
+        </div>
+      ))}
+
+      <h2 className="pb-2 pt-8">Dite</h2>
+      {data.documents.map((el) => (
+        <div key={el.id} className="pb-2">
+          <Link to={`/details`}>{el.name}</Link>
+        </div>
+      ))}
+      <h2 className="pb-2 pt-4">Goals</h2>
+      {data.documents.map((el) => (
+        <div key={el.id} className="pb-2">
+          <Link to={`/details`}>{el.name}</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export const catLoader = async () => {
-  const res = await fetch("http://localhost:8080/categories")
+  const res = await fetch("http://localhost:8080/categories");
 
-  if(!res.ok) {
-    throw new Error("Something went wrong")
+  if (!res.ok) {
+    throw new Error("Something went wrong");
   }
-  return res.json()
-}
+  return res.json();
+};
