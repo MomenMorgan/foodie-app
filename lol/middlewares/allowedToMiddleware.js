974d089c5739d2d015bbc9ef6ad4c4d122c887ae
momen morgan <mominmorgann10@gmail.com>
2024-04-24 22:11:08 +0200

    Hot fix

--- lol/middlewares/allowedToMiddleware.js
+++
@@ -1,9 +0,0 @@
-const errorHelper = require('../utils/error');
-
-module.exports = (...roles) =>
-  (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      errorHelper('You are not allowed to access this route.', 403);
-    }
-    next();
-  }
